17d44e30ccc56231a5ed7f6f453f33fb8c9d9cc6
noi-leistner <elvarnoi.leistner@students.salle.url.edu>
2026-09-26 18:04:21 +0200

    parse voyages

--- Ithaca.c
+++ Ithaca.c
@@ -43,7 +43,77 @@ Ithaca parseIthaca(char *config_path) {
     return ithaca;
 }
 
+Voyage *parseVoyages(char *voyages_path, int *num_voyages) {
+    int fd = open(voyages_path, O_RDONLY);
+    if (fd < 0) {
+        //TODO: make real error message
+        exit(EXIT_FAILURE);
+    }
+
+    // Read whole file into buffer
+    char buf[4000]; //TODO: make dynamic
+    int total = 0, n;
+    while ((n = read(fd, &buf[total], sizeof(buf) - total - 1)) > 0) {
+        total += n;
+    }
+    close(fd);
+
+    if (n < 0) {
+        write(STDERR_FILENO, "Error: failed reading voyages file\n", 36);
+        exit(EXIT_FAILURE);
+    }
+    buf[total] = '\0';
+
+    // First pass: count lines
+    char buf_copy[4000];
+    strncpy(buf_copy, buf, sizeof(buf_copy) - 1);
+    buf_copy[sizeof(buf_copy) - 1] = '\0';
+
+    int count = 0;
+    char *line = strtok(buf_copy, "\n");
+    while (line != NULL) {
+        count++;
+        line = strtok(NULL, "\n");
+    }
+
+    Voyage *voyages = malloc(sizeof(Voyage) * count);
+    if (!voyages && count > 0) {
+        write(STDERR_FILENO, "Error: malloc failed\n", 21);
+        exit(EXIT_FAILURE);
+    }
+    
+    // Second pass: parse each line into a Voyage
+    line = strtok(buf, "\n");
+    int i = 0;
+    while (line != NULL) {
+        char *tok = strtok(line, " ");
+        strncpy(voyages[i].object, tok, sizeof(voyages[i].object) - 1);
+        voyages[i].object[sizeof(voyages[i].object) - 1] = '\0';
+
+        tok = strtok(NULL, " ");
+        strncpy(voyages[i].file, tok, sizeof(voyages[i].file) - 1);
+        voyages[i].file[sizeof(voyages[i].file) - 1] = '\0';
+
+        tok = strtok(NULL, " ");
+        strncpy(voyages[i].destination, tok, sizeof(voyages[i].destination) - 1);
+        voyages[i].destination[sizeof(voyages[i].destination) - 1] = '\0';
+
+        tok = strtok(NULL, " ");
+        voyages[i].reward = atoi(tok);
+
+        line = strtok(NULL, "\n");
+        i++;
+    }
+
+    *num_voyages = count;
+    return voyages;
+}
+
 int main(int argc, char *argv[]) {
+    Ithaca ithaca;
+    Voyage *voyages;
+    int num_voyages;
+
     if (argc != 3) {
         //TODO: make real error message
         write(STDERR_FILENO, "Usage: ./ithaca <config.dat> <voyages.dat>\n", 44);
@@ -53,5 +123,7 @@ int main(int argc, char *argv[]) {
     char *config_path = argv[1];
     char *voyages_path = argv[2];
 
-    Ithaca ithaca = parseIthaca(config_path);
+    ithaca = parseIthaca(config_path);
+
+    voyages = parseVoyages(voyages_path, &num_voyages);
 }
